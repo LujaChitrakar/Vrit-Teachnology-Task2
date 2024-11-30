6beea3f9bcf2084c50aa72ae3d029a7cad70248a
Luja Chitrakar <lujachitrakar156@gmail.com>
2024-11-30 11:59:12 +0545

    minor bg changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import Navbar from "./components/common/Navbar";
+// import backgroundImage from "./assets/mountain.jpg";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <div
+      // style={{
+      //   backgroundImage: `url(${backgroundImage})`,
+      //   backgroundSize: "cover",
+      //   backgroundPosition: "center",
+      //   height: "100vh",
+      // }}
+      >
+        <Navbar />
+      </div>
     </>
   );
 }
