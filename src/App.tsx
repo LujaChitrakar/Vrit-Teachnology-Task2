d2416e44e597bdb690c15310fec96461dbee35ab
Luja Chitrakar <lujachitrakar156@gmail.com>
2024-11-30 11:24:16 +0545

    setting up project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+
+function App() {
+  return (
+    <>
+      <div className=" bg-slate-600 text-white">
+        HI
+        <FontAwesomeIcon icon={faFilter} />
+      </div>
+    </>
+  );
+}
+
+export default App;
